974829c45df7ab84569f462e1199482c54a7ee63
Estian <estianyssel@gmail.com>
2020-10-12 13:44:11 +0200

    Adding Main Menu and Scene Navigation

---
+++ Assets/Scripts/Main Menu/UI_Controller.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.SceneManagement;
+
+public class UI_Controller : MonoBehaviour
+{
+
+    public Button btnGameOfLife;
+    // Start is called before the first frame update
+    void Start()
+    {
+        Button btn = btnGameOfLife.GetComponent<Button>();
+        btn.onClick.AddListener(TaskOnClick);
+    }
+
+    void TaskOnClick()
+    {
+        SceneManager.LoadScene("Conways Game Of Life");
+    }
+}
