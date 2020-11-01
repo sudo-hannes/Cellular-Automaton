4f964c0a5502bcb36b2402e3d3c695ed0e9b1c95
Estian <estianyssel@gmail.com>
2020-11-01 13:38:35 +0200

    Updated Game Of Life UI; Committed Langtons Ant.unity changes

--- Assets/Scripts/Conway's Game Of Life/UI_Controller_GameOfLIfe.cs
+++ Assets/Scripts/Conway's Game Of Life/UI_Controller_GameOfLIfe.cs
@@ -2,14 +2,22 @@
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.SceneManagement;
+using UnityEngine.UI;
 
 public class UI_Controller_GameOfLIfe : MonoBehaviour
 {
-    
+    public Button infoButton;
+    public GameObject infoText;
+    private bool infoShown = false;
+    public Button exitButton;
     // Start is called before the first frame update
     void Start()
     {
-        
+        Button btnInfo = infoButton.GetComponent<Button>();
+        btnInfo.onClick.AddListener(ShowInfo);
+
+        Button btnExit = exitButton.GetComponent<Button>();
+        btnExit.onClick.AddListener(ExitToMain);
     }
 
     // Update is called once per frame
@@ -19,5 +27,16 @@ public class UI_Controller_GameOfLIfe : MonoBehaviour
         {
             SceneManager.LoadScene("Main Menu");
         }
+    }
+
+    void ExitToMain()
+    {
+        SceneManager.LoadScene("Main Menu");
+    }
+
+    void ShowInfo()
+    {
+        infoShown = !infoShown;
+        infoText.SetActive(infoShown);
     }
 }
